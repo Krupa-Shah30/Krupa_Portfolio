2b77b641a70f744ab2f5a42df2a44f9cb296d3dc
krupa shah <shahkrupa903@gmail.com>
2025-09-15 16:17:44 -0500

    Updates

---
+++ _next/static/chunks/app/layout-4bef417940780765.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5003:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}},8050:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,5003,23))}},e=>{e.O(0,[211,441,255,358],()=>e(e.s=8050)),_N_E=e.O()}]);
